4d5ef0340a2b6b944ccf157647e10538c62a5ab1
Yansuputod <arta70660@gmail.com>
2025-03-01 20:43:43 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Laravel-12']);
+});
+
+?>
