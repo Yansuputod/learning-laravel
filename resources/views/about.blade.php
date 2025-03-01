cd57d9025f0d5eff08d785216aa603a707620717
Yansuputod <arta70660@gmail.com>
2025-03-01 20:43:43 +0800

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>about</title>
+</head>
+<body>
+    <h1>About</h1>
+    <p>This is the about page</p>
+    <h2>data: {{ $name }}</h2>
+    <br>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
